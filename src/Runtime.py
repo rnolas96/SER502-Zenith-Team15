f8a34fd97b55a0d7feeeb2f65ec22429c82e371f
arvindanand2 <166462772+arvindanand2@users.noreply.github.com>
2024-04-24 21:24:37 -0700

    Add files via upload
    IntermediateCode and runtime upload

---
+++ src/Runtime.py
@@ -0,0 +1,13 @@
+class Runtime:
+    variables = {}
+    def init(self):
+        self.variables = {}
+
+    def execute(self, intermediate_code):
+        # Split the intermediate code into lines
+        lines = intermediate_code.split("\n")
+
+        for line in lines:
+            line = line.strip()
+            if line:
+                exec(line, {}, self.variables)
